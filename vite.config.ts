d73de21f70909c2fcfc74a04545e6a131991fb06
CodeSandbox <support@codesandbox.io>
2026-03-20 03:45:37 +0000

    Sandbox snapshot commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
+
+// vite.config.ts
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -10,5 +12,11 @@ export default defineConfig({
       "65575t-5174.csb.app",
       "65575t-5175.csb.app",
     ],
+    proxy: {
+      '/api': {
+        target: 'http://43.106.102.92:3000', 
+        changeOrigin: true,
+      }
+    }
   },
 });
